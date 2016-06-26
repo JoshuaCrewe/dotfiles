ead97d80cc9b1f6019e4b51d86ae979dc9a74275
Joshua Crewe <me+git@joshuacrewe.co.uk>
2016-06-26 10:04:16 +0100

    add some basic functionality to hs config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,21 @@
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.notify.new({title="Hammerspoon", informativeText="Hello Nancy"}):send()
 end)
+
+-- hotkey mash
+local mash   = {"ctrl", "cmd"}
+local mash_app   = {"cmd", "alt", "ctrl", "shift"}
+local mash_shift = {"ctrl", "cmd", "shift"}
+
+-- disable animation
+hs.window.animationDuration = 0
+
+-- Reload config
+hs.hotkey.bind(mash, "R", function()
+  hs.reload()
+end)
+
+-- Window Hints
+hs.hotkey.bind(mash, '.', hs.hints.windowHints)
+
+hs.hotkey.bind(mash_app, 'W', function () hs.application.launchOrFocus("Dictionary") end)
