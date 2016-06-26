4169d8d4bfbe5caca71c260535037aeb9d00fac5
Joshua Crewe <me+git@joshuacrewe.co.uk>
2016-06-26 10:19:36 +0100

    Add an autoreload to Hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -10,12 +10,28 @@ local mash_shift = {"ctrl", "cmd", "shift"}
 -- disable animation
 hs.window.animationDuration = 0
 
+-- Window Hints
+hs.hotkey.bind(mash, '.', hs.hints.windowHints)
+
+hs.hotkey.bind(mash_app, 'W', function () hs.application.launchOrFocus("Dictionary") end)
+
 -- Reload config
 hs.hotkey.bind(mash, "R", function()
   hs.reload()
 end)
 
--- Window Hints
-hs.hotkey.bind(mash, '.', hs.hints.windowHints)
+-- watch config for changes and reload when they occur
+function reloadConfig(files)  
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
 
-hs.hotkey.bind(mash_app, 'W', function () hs.application.launchOrFocus("Dictionary") end)
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()  
+-- hs.alert.show("Config reloaded")  
