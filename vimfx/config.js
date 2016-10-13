d19dc48d63c227affb73643d7fcad120e1d5d4c5
Joshua Crewe <joshua@venncreative.co.uk>
2016-10-13 08:57:43 +0100

    Add missing semi-colons etc

--- vimfx/config.js
+++ vimfx/config.js
@@ -5,26 +5,26 @@ const nsIStyleSheetService = Cc['@mozilla.org/content/style-sheet-service;1'].ge
 let {commands} = vimfx.modes.normal
 
 let map = (shortcuts, command, custom=false) => {
-  vimfx.set(`${custom ? 'custom.' : ''}mode.normal.${command}`, shortcuts)
-}
+  vimfx.set(`${custom ? 'custom.' : ''}mode.normal.${command}`, shortcuts);
+};
 
 let set = (pref, valueOrFunction) => {
   let value = typeof valueOrFunction === 'function'
     ? valueOrFunction(vimfx.getDefault(pref))
     : valueOrFunction
-  vimfx.set(pref, value)
-}
+  vimfx.set(pref, value);
+};
 
 let loadCss = (uriString) => {
-  let uri = Services.io.newURI(uriString, null, null)
+  let uri = Services.io.newURI(uriString, null, null);
   let method = nsIStyleSheetService.AUTHOR_SHEET
   if (!nsIStyleSheetService.sheetRegistered(uri, method)) {
     nsIStyleSheetService.loadAndRegisterSheet(uri, method)
   }
   vimfx.on('shutdown', () => {
-    nsIStyleSheetService.unregisterSheet(uri, method)
+    nsIStyleSheetService.unregisterSheet(uri, method);
   })
-}
+};
 
 let toggleCss = (uriString) => {
     let uri = Services.io.newURI(uriString, null, null)
@@ -32,54 +32,54 @@ let toggleCss = (uriString) => {
     if (nsIStyleSheetService.sheetRegistered(uri, method)) {
         nsIStyleSheetService.unregisterSheet(uri, method)
     } else {
-        nsIStyleSheetService.loadAndRegisterSheet(uri, method)
-    }
+        nsIStyleSheetService.loadAndRegisterSheet(uri, method);
+    };
     vimfx.on('shutdown', () => {
-        nsIStyleSheetService.unregisterSheet(uri, method)
-    })
-}
+        nsIStyleSheetService.unregisterSheet(uri, method);
+    });
+};
 
 // Mappings
 
 // Remove some mappings
-map(' ', 'scroll_page_down')
+map(' ', 'scroll_page_down');
 
 // Tabs
-map('(',  'tab_select_previous')
-map(')',  'tab_select_next')
-map('d',  'tab_close')
-map('u',  'tab_restore')
-map('<space><space>', 'tab_select_most_recent')
-map('<c-e>', 'tab_select_last')
-map('<c-a>', 'tab_select_first_non_pinned')
+map('(',  'tab_select_previous');
+map(')',  'tab_select_next');
+map('d',  'tab_close');
+map('u',  'tab_restore');
+map('<space><space>', 'tab_select_most_recent');
+map('<c-e>', 'tab_select_last');
+map('<c-a>', 'tab_select_first_non_pinned');
 
 // Scrolling
-map('`', 'scroll_to_mark')
-map('<c-d>', 'scroll_half_page_down')
-map('<c-u>', 'scroll_half_page_up')
+map('`', 'scroll_to_mark');
+map('<c-d>', 'scroll_half_page_down');
+map('<c-u>', 'scroll_half_page_up');
 
 // Hints
-map('gf', 'click_browser_element')
+map('gf', 'click_browser_element');
 
 // Searching
-map('/', 'find_highlight_all')
-map('a/', 'find')
-map('n', 'find_next')
-map('N', 'find_previous')
+map('/', 'find_highlight_all');
+map('a/', 'find');
+map('n', 'find_next');
+map('N', 'find_previous');
 
 // Other ?
-map('y', 'copy_current_url')
+map('y', 'copy_current_url');
 
 // Preferences
 
-set('hints.chars', '1234567890')
-set('prevent_autofocus', true)
-set('prev_patterns', v => `föregående  ${v}`)
-set('next_patterns', v => `nästa  ${v}`)
-set('counts_enabled', true)
+set('hints.chars', '1234567890');
+set('prevent_autofocus', true);
+set('prev_patterns', v => `föregående  ${v}`);
+set('next_patterns', v => `nästa  ${v}`);
+set('counts_enabled', true);
 
 // Some preferences are used to edit Built in firefox things
-let {Preferences} = Cu.import('resource://gre/modules/Preferences.jsm', {})
+let {Preferences} = Cu.import('resource://gre/modules/Preferences.jsm', {});
 
 Preferences.set({
   'accessibility.blockautorefresh': true,
@@ -98,7 +98,7 @@ Preferences.set({
   'privacy.donottrackheader.enabled': true,
   'toolkit.scrollbox.verticalScrollDistance' : 4,
   'extensions.VimFx.smoothscroll.lines.spring-constant' : 1000,
-})
+});
 
 // Custom Commands
  vimfx.addCommand({
@@ -108,15 +108,15 @@ Preferences.set({
   order: commands.tab_move_forward.order + 1,
 }, ({vim, count}) => {
   if (count === undefined) {
-    vim.notify('Provide a count')
-    return
+    vim.notify('Provide a count');
+    return;
   }
   let {window} = vim
   window.setTimeout(() => {
-    window.gBrowser.moveTabTo(window.gBrowser.selectedTab, count - 1)
-  }, 0)
-})
-map('gt', 'tab_move_to_index', true)
+    window.gBrowser.moveTabTo(window.gBrowser.selectedTab, count - 1);
+  }, 0);
+});
+map('gt', 'tab_move_to_index', true);
 
 vimfx.addCommand({
   name: 'search_bookmarks',
@@ -124,10 +124,10 @@ vimfx.addCommand({
   category: 'location',
   order: commands.focus_location_bar.order + 1,
 }, (args) => {
-  commands.focus_location_bar.run(args)
-  args.vim.window.gURLBar.value = '* '
-})
-map('<space><s-CR>', 'search_bookmarks', true)
+  commands.focus_location_bar.run(args);
+  args.vim.window.gURLBar.value = '* ';
+});
+map('<space><s-CR>', 'search_bookmarks', true);
 
 vimfx.addCommand({
   name: 'search_history',
@@ -135,10 +135,10 @@ vimfx.addCommand({
   category: 'location',
   order: commands.focus_location_bar.order + 1,
 }, (args) => {
-  commands.focus_location_bar.run(args)
-  args.vim.window.gURLBar.value = '^ '
-})
-map('<space>h', 'search_history', true)
+  commands.focus_location_bar.run(args);
+  args.vim.window.gURLBar.value = '^ ';
+});
+map('<space>h', 'search_history', true);
 
 vimfx.addCommand({
   name: 'search_tabs',
@@ -146,26 +146,26 @@ vimfx.addCommand({
   category: 'location',
   order: commands.focus_location_bar.order + 1,
 }, (args) => {
-  commands.focus_location_bar.run(args)
-  args.vim.window.gURLBar.value = '% '
-})
-map('<space><CR>', 'search_tabs', true)
+  commands.focus_location_bar.run(args);
+  args.vim.window.gURLBar.value = '% ';
+});
+map('<space><CR>', 'search_tabs', true);
 
 vimfx.addCommand({
   name: 'web_console',
   description: 'Web console',
 }, ({vim}) => {
-  vim.window.gDevToolsBrowser.selectToolCommand(vim.window.gBrowser, 'webconsole')
-})
-map('gk', 'web_console', true)
+  vim.window.gDevToolsBrowser.selectToolCommand(vim.window.gBrowser, 'webconsole');
+});
+map('gk', 'web_console', true);
 
 vimfx.addCommand({
   name: 'web_inspector',
   description: 'Web inspector',
 }, ({vim}) => {
   vim.window.gDevToolsBrowser.toggleToolboxCommand(vim.window.gBrowser)
-})
-map('<c-s>', 'web_inspector', true)
+});
+map('<c-s>', 'web_inspector', true);
 
 vimfx.addCommand({
     name: 'focus_unhighlighted_location_bar',
@@ -177,23 +177,23 @@ vimfx.addCommand({
     let active = args.vim.window.document.activeElement;
     active.selectionStart = active.selectionEnd;
 });
-map('O', 'focus_unhighlighted_location_bar', true)
+map('O', 'focus_unhighlighted_location_bar', true);
 
 vimfx.addCommand({
     name: 'goto_addons',
     description: 'Addons',
 }, ({vim}) => {
     vim.window.BrowserOpenAddonsMgr()
-})
-map('<space>a', 'goto_addons', true)
+});
+map('<space>a', 'goto_addons', true);
 
 vimfx.addCommand({
     name: 'goto_config',
     description: 'Config',
 }, ({vim}) => {
     vim.window.switchToTabHavingURI('about:config', true)
-})
-map('<space>c', 'goto_config', true)
+});
+map('<space>c', 'goto_config', true);
 
 vimfx.addCommand({
     name: 'goto_downloads',
@@ -201,24 +201,24 @@ vimfx.addCommand({
 }, ({vim}) => {
     // vim.window.switchToTabHavingURI('about:downloads', true)
     vim.window.DownloadsPanel.showDownloadsHistory()
-})
-map('<space>d', 'goto_downloads', true)
+});
+map('<space>d', 'goto_downloads', true);
 
 vimfx.addCommand({
     name: 'midnight',
     description: 'Midnight Surfing',
 }, ({vim}) => {
     toggleCss(`${__dirname}/custom-css/midnight.css`)
-})
-map('<space>n', 'midnight', true)
+});
+map('<space>n', 'midnight', true);
 
 vimfx.addCommand({
     name: 'pocket',
     description: 'Save to Pocket',
 }, ({vim}) => {
     vim.window.document.getElementById('pocket-button').click();
-});
-map('s', 'pocket', true)
+});;
+map('s', 'pocket', true);
 
 // This is the javascript which runs what the font
 // javascript:(function(){var%20d=document,s=d.createElement('scr'+'ipt'),b=d.body,l=d.location;s.setAttribute('src','http://chengyinliu.com/wf.js?o='+encodeURIComponent(l.href)+'&t='+(new%20Date().getTime()));b.appendChild(s)})();
