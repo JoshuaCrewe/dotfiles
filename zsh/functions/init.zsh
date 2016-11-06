de84b53fa0a92b9fcab9a6773e4a5c99e6d24faa
Joshua Crewe <me+git@joshuacrewe.co.uk>
2016-11-06 14:31:06 +0000

    I dont think I am going to use that function

--- zsh/functions/init.zsh
+++ zsh/functions/init.zsh
@@ -8,26 +8,6 @@ function cdls {
   builtin cd "$argv[-1]" && ls "${(@)argv[1,-2]}"
 }
 
-function tmux-neww-in-cwd() {
-    SIP=$(tmux display-message -p "#S:#I:#P")
-
-    PTY=$(tmux server-info |
-    egrep flags=\|bytes |
-    awk '/windows/ { s = $2 }
-    /references/ { i = $1 }
-    /bytes/ { print s i $1 $2 } ' |
-    grep "$SIP" |
-    cut -d: -f4)
-
-    PTS=${PTY#/dev/}
-
-    PID=$(ps -eao pid,tty,command --forest | awk '$2 == "'$PTS'" {print $1; exit}')
-
-    DIR=$(readlink /proc/$PID/cwd)
-
-    tmux neww "cd '$DIR'; $SHELL"
-}
-
 # Search through Firefox history ( work only at the moment )
 function fox() {
   local cols sep
