4fe8963f31a75acde7ea5cfa5774453740cbae3f
Joshua Crewe <me+git@joshuacrewe.co.uk>
2017-03-26 15:45:28 +0100

    Clean up  backup configuration

--- zsh/plugins/init.zsh
+++ zsh/plugins/init.zsh
@@ -1,28 +1,8 @@
-# if [[ -f ~/.zpm/zpm.zsh ]]; then
-#     source ~/.zpm/zpm.zsh
+# if [[ -f /usr/local/bin/notes ]]; then
+#     
 # else
-#     git clone --recursive https://github.com/horosgrisa/zpm ~/.zpm
-#     source ~/.zpm/zpm.zsh
-#   fi
-#
-# Plug mafredri/zsh-async                         # For the pure promt to work
-# Plug sindresorhus/pure                          # A minimalist prompt
-#
-# Plug urbainvaes/fzf-marks                       # Bookmarking with fzf filtering
-# Plug b4b4r07/zsh-gomi                           # Move things to system trash
-# Plug zsh-users/zsh-completions
-#
-# Plug git
-# Plug genpasswd
-#
-# Plug zsh-users/zsh-syntax-highlighting
-
-# I was goint to try notes but I am not so sure.
-if [[ -f /usr/local/bin/notes ]]; then
-    
-else
-    curl https://cdn.rawgit.com/pimterry/notes/v0.2.0/notes > /usr/local/bin/notes && chmod +x /usr/local/bin/notes
-fi
+#     curl https://cdn.rawgit.com/pimterry/notes/v0.2.0/notes > /usr/local/bin/notes && chmod +x /usr/local/bin/notes
+# fi
 
 # Check if zplug is installed
 if [[ ! -d ~/.zplug ]]; then
@@ -41,6 +21,10 @@ zplug "zsh-users/zsh-completions"
 
 zplug "mafredri/zsh-async"                         # For the pure promt to work
 zplug "sindresorhus/pure"                          # A minimalist prompt
+
+zplug "pimterry/notes", \
+    as:command, \
+    use:notes
 
 zplug "witt3rd/894c9e0b9ca4e24e5574", \
     from:gist, \
