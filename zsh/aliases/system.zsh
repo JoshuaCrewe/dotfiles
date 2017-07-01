0ce84dc16796b7705b0f9cd3c86cd6712903902e
Joshua Crewe <me+git@joshuacrewe.co.uk>
2017-07-01 11:09:59 +0100

    Organise aliases

---
+++ zsh/aliases/system.zsh
@@ -0,0 +1,34 @@
+# system {{
+    if [[ $IS_MAC == 1 ]]; then
+        alias show='defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder'  # see the hidden thoughts!
+        alias hide='defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder'  # keep hidden things hidden!
+    fi
+
+    # Mac OS X Everywhere
+    if [[ $IS_MAC == 1 ]]; then
+      alias o='open'
+    elif [[ "$OSTYPE" == cygwin* ]]; then
+      alias o='cygstart'
+      alias pbcopy='tee > /dev/clipboard'
+      alias pbpaste='cat /dev/clipboard'
+    else
+      alias o='xdg-open'
+
+      if (( $+commands[xclip] )); then
+        alias pbcopy='xclip -selection clipboard -in'
+        alias pbpaste='xclip -selection clipboard -out'
+      elif (( $+commands[xsel] )); then
+        alias pbcopy='xsel --clipboard --input'
+        alias pbpaste='xsel --clipboard --output'
+      fi
+    fi
+
+    # Homebrew
+    alias brewu='brew update && brew upgrade && brew cleanup && brew prune && brew doctor'
+
+    # Gomi
+    if [[ $IS_MAC == 1 ]]; then
+        alias gomi='gomi -s'
+        alias trash='gomi -s'
+    fi
+# }}
