23411468793ebc3c70bb1164ead425614c605907
Joshua Crewe <joshua@venncreative.co.uk>
2017-09-21 14:51:12 +0100

    Switch to neovim

--- zsh/aliases/vim.zsh
+++ zsh/aliases/vim.zsh
@@ -1,4 +1,6 @@
 # Vim {{
+    alias vim='nvim'
+    alias vi='nvim'
     alias vimrc='vim -c e ~/dotfiles/vimrc'
     alias zshrc='vim -c e ~/dotfiles/zshrc'
     alias tmuxconf='vim -c e ~/dotfiles/tmux.conf'
@@ -7,4 +9,7 @@
     alias zsh-plugins='vim -c e ~/dotfiles/zsh/plugins/init.zsh'
     alias zsh-functions='vim -c e ~/dotfiles/zsh/functions/init.zsh'
     alias zsh-options='vim -c e ~/dotfiles/zsh/options/init.zsh'
+
+    # Shhh dont tell anyone
+    alias ed='emacs'
 # }}
