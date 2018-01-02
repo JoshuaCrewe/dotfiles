3f3ef5ee159dc1ddd32677b484f050fb71178eef
Joshua Crewe <me+git@joshuacrewe.co.uk>
2018-01-02 20:59:08 +0000

    Add notes plugin
    Also light load everything for speed?

--- zsh/plugins/zplugin.zsh
+++ zsh/plugins/zplugin.zsh
@@ -9,21 +9,20 @@ autoload -Uz _zplugin
 (( ${+_comps} )) && _comps[zplugin]=_zplugin
 ### End of Zplugin's installer chunk
 
-zplugin load urbainvaes/fzf-marks                       # Bookmarking with fzf filtering
+zplugin light urbainvaes/fzf-marks                       # Bookmarking with fzf filtering
 
-zplugin load b4b4r07/zsh-gomi                           # Move things to system trash
-zplugin load zsh-users/zsh-completions
+zplugin light b4b4r07/zsh-gomi                           # Move things to system trash
+zplugin light zsh-users/zsh-completions
 
-zplugin load MichaelAquilina/zsh-you-should-use
+zplugin light MichaelAquilina/zsh-you-should-use
 
-zplugin load mafredri/zsh-async
-zplugin load sindresorhus/pure
+zplugin light mafredri/zsh-async
+zplugin light sindresorhus/pure
 
-# This is not installed well with zplugin
-# It is not something which I particularly love using
 # zplugin load pimterry/notes
+zplugin snippet --command https://github.com/pimterry/notes/blob/master/notes
 
-zplugin load paulirish/git-open
+zplugin light paulirish/git-open
 
 zplugin light zdharma/fast-syntax-highlighting
 
