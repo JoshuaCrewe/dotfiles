e5dd7b25be4d0474084dd3b48971eeb185ef6848
Joshua Crewe <joshua@venncreative.co.uk>
2018-07-17 16:46:19 +0100

    Move iterms config to dotfiles root

---
+++ iterm/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+[ "$(uname -s)" != "Darwin" ] && exit 0
+
+defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string "$DOTFILES/iterm"
+defaults write com.googlecode.iterm2 "LoadPrefsFromCustomFolder" -bool true
