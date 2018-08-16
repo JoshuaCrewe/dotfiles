9ffcdba6eec3d7d9b23c33b6f6cb4e8b9406beaf
Joshua Crewe <joshua@venncreative.co.uk>
2018-08-16 17:56:21 +0100

    Update install boiler plate options

--- scripts/_shared.sh
+++ scripts/_shared.sh
@@ -3,6 +3,17 @@
 set -u # error out if variable is not set
 set -e # exit if returns non true
 
+is_linux=0
+is_mac=0
+
+if [[ $(uname) = 'Linux' ]]; then
+    is_linux=1
+fi
+
+if [[ $(uname) = 'Darwin' ]]; then
+    is_mac=1
+fi
+
 _info() {
 	# shellcheck disable=SC2059
 	printf "\r  [ \033[00;34m..\033[0m ] $1\n"
@@ -13,9 +24,19 @@ _user() {
 	printf "\r  [ \033[0;33m??\033[0m ] $1\n"
 }
 
+_warn() {
+	# shellcheck disable=SC2059
+	printf "\r  [ \033[0;33mWARN\033[0m ] $1\n"
+}
+
 _success() {
 	# shellcheck disable=SC2059
-	printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+	printf "\r\033[2K  [ \033[00;32mDONE\033[0m ] $1\n"
+}
+
+_backup() {
+	# shellcheck disable=SC2059
+	printf "\r\033[2K  [ \033[00;34mBACKUP\033[0m ] $1\n"
 }
 
 _enable() {
@@ -31,18 +52,24 @@ _fail() {
 }
 
 _link () {
-	if [ -f "$2" ]; then
-        mv "$2" "$2.bak"
-        _success "moved $2 to $2.bak"
+	if [ -f "$2" ] || [ -d "$2" ]; then
+        if mv "$2" "$2.bak" 2>/dev/null ; then
+            _backup "moved $2 to $2.bak"
+        else
+            _warn "Backup $2 already exists"
+        fi
 	fi
 	ln -sf "$1" "$2"
 	_success "linked $1 to $2"
 }
 
 _sudo_link () {
-	if [ -f "$2" ]; then
-        sudo mv "$2" "$2.bak"
-        _success "moved $2 to $2.bak"
+	if [ -f "$2" ] || [ -d "$2" ]; then
+        if sudo  mv "$2" "$2.bak" 2>/dev/null ; then
+            _backup "moved $2 to $2.bak"
+        else
+            _warn "Backup $2 already exists"
+        fi
 	fi
 	sudo ln -sf "$1" "$2"
 	_success "linked $1 to $2"
