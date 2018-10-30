4a9083e1a8c02c695a71c37d7f972b5fa932db31
Joshua Crewe <joshua@venncreative.co.uk>
2018-10-30 13:49:50 +0000

    Update macos install scripts

--- macos/install.sh
+++ macos/install.sh
@@ -6,7 +6,15 @@ set -e # exit if returns non true
 dirname=$(cd "$(dirname "$0")"; pwd)
 source "$dirname/../scripts/_shared.sh"
 
+if [[ $is_mac == 1 ]]; then
 
-# if [[ $is_mac == 1 ]]; then
-    # sh -c "$(dirname "$0")/install-macos.sh"
-# fi
+	echo "› Brew Bundle..."
+    if hash brew 2>/dev/null; then
+        cd $dirname
+        brew bundle
+        cd -
+    fi
+
+	echo "› set up mac defaults..."
+    sh -c "macos"
+fi
