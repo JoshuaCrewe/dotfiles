9e51867904b485033ac605c77383adbd053bfb78
Joshua Crewe <joshua@venncreative.co.uk>
2019-05-22 16:17:43 +0100

    Tidy up zsh exports config

--- zsh/options/exports.zsh
+++ zsh/options/exports.zsh
@@ -1,5 +1,5 @@
 # set what gets put in PATH
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin:${HOME}/.local/bin:${HOME}/dotfiles/bin:$PATH"
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin:${HOME}/.local/bin:$PATH"
 
 # Use Brew curl
 export PATH="/usr/local/opt/curl/bin:$PATH"
@@ -7,10 +7,11 @@ export PATH="/usr/local/opt/curl/bin:$PATH"
 # Use Brew php
 export PATH="/usr/local/opt/php@7.2/bin:$PATH"
 
+# Load dotfile development Environment
 export DOTFILES="$HOME/dotfiles"
+export PATH="${HOME}/dotfiles/bin:$PATH"
 
 # Use The Silver Searcher for fzf
-
 if [[ -x `which fzf` ]]; then
     if [[ -x `which fd` ]]; then
         export FZF_DEFAULT_COMMAND='fd --hidden -i -E "node_modules"'
@@ -41,8 +42,6 @@ export LSCOLORS="Gxfxcxdxbxegedabagacad"
 # export GREP_OPTIONS='--color=auto'
 # export GREP_COLOR='3;33'
 
-# export NOTES_DIRECTORY=~/Dropbox/notes/
-
 # Dont check internal mail.
 # This will stop the "you've got mail" message when opening a terminal
 unset MAILCHECK
@@ -54,6 +53,7 @@ export PATH=$GOPATH/bin:$PATH
 # Set up cargo/rust path
 export PATH="${HOME}/.cargo/bin:$PATH"
 
+# Always choose vim to edit with
 export EDITOR=$(which vim)
 
 export GPG_TTY=$(tty) 
@@ -62,4 +62,5 @@ if hash nnn 2>/dev/null; then
     export NNN_USE_EDITOR=1
 fi
 
+# Stop homebrew trying to access Github
 export HOMEBREW_NO_GITHUB_API=1
