ce181bcc82bd844e9b143b80162104f7a56072a3
Joshua Crewe <joshua@venncreative.co.uk>
2019-10-25 15:48:57 +0100

    Update Hammerspoon approach

---
+++ hammerspoon/hammerspoon.symlink/__example/init.lua
@@ -0,0 +1,9 @@
+local module = { cache = cache }
+
+module.start = function()
+end
+
+module.stop = function()
+end
+
+return module
--- hammerspoon/install.sh
+++ hammerspoon/install.sh
@@ -10,6 +10,12 @@ if [[ $is_mac == 1 ]]; then
     src="$dirname/hammerspoon.symlink"
     dst="$HOME/.hammerspoon"
     _link "$src" "$dst"
+    if hash curl 2>/dev/null; then
+        curl -LO https://github.com/Hammerspoon/Spoons/raw/master/Spoons/SpoonInstall.spoon.zip && \n
+        unzip SpoonInstall.spoon.zip && \n
+        mkdir -p $HOME/dotfiles/hammerspoon.symlink/Spoons && \n
+        mv SpoonInstall.spoon $HOME/dotfiles/hammerspoon.symlink/Spoons
+    fi
 
     if hash brew 2>/dev/null; then
         if [[ ! -d /Applications/Hammerspoon.app ]]; then
