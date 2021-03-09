e44b7d1330091689d8b42162604ad81dfdff44eb
Joshua Crewe <git@joshuacrewe.co.uk>
2021-03-09 16:14:02 +0000

    Finally found that systemd service for DFK

--- dual-function-keys/install.sh
+++ dual-function-keys/install.sh
@@ -8,6 +8,8 @@ source "$dirname/../scripts/_shared.sh"
 
 
 if [[ $is_linux == 1 ]]; then
+    _aur interception-dual-function-keys
+
     src="$dirname/dual-function-keys.yaml.symlink"
     dst="$HOME/.config/dual-function-keys/dual-function-keys.yaml"
     _link "$src" "$dst"
@@ -16,5 +18,10 @@ if [[ $is_linux == 1 ]]; then
     dst="/etc/interception/udevmon.yaml"
     _sudo_link "$src" "$dst"
 
-    _aur interception-dual-function-keys
+    src="$dirname/udevmon.service"
+    dst="/etc/systemd/system/udevmon.service"
+    chmod 644 "$src"
+    _sudo_link "$src" "$dst"
+    _sudo_enable 'udevmon.service'
 fi
+
